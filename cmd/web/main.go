573ae696d2f30ea155fbab33e323be4d76fd817a
Beknur1003 <beka2000beka@gmail.com>
2021-11-02 15:19:21 +0600

    Initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"github.com/Beknur1003/bookings/pkg/config"
+	"github.com/Beknur1003/bookings/pkg/handlers"
+	"github.com/Beknur1003/bookings/pkg/render"
+	"fmt"
+	"github.com/alexedwards/scs/v2"
+	"log"
+	"net/http"
+	"time"
+)
+
+const portNumber = ":8080"
+
+var app config.AppConfig
+var session *scs.SessionManager
+
+func main() {
+	// change this to TRUE when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 254 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("Cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+}
